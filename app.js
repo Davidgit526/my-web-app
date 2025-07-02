a50cfea3b8108c27b8b7bc818bb9248d4c986815
Y David Prabhakar <149075785+Davidgit526@users.noreply.github.com>
2025-07-02 13:39:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the AWS CodePipeline/CodeDeploy CI/CD pipeline demo!');
+  res.send('Our Story
+Explore our various services that help to gow your business In a world brimming with possibilities, we sprouted, seizing our destiny to create an extraordinary narrative - a tale that encapsulates laughter, tears, challenges, and triumphs.!');
 });
 
 app.listen(port, '0.0.0.0', () => {
