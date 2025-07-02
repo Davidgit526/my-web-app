c994c420006c91f6d520b1baf9d66932444f9307
Y David Prabhakar <149075785+Davidgit526@users.noreply.github.com>
2025-07-02 12:39:07 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
 echo "Installing Node.js dependencies"
-cd /home/ec2-user/myapp
+
+# Install Node.js and npm (Node.js 16)
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+
+# Go to the app directory
+cd /home/ec2-user/myapp || exit 1
+
+# Install dependencies
 npm install
